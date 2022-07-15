84182e604052eb43a7d2f44db3d3a3ae9f94a688
bulutyelken <190101052@ogrenci.yalova.edu.tr>
2022-07-15 16:04:12 +0300

    favicon

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,7 +9,7 @@ function Header({setAdd}){
                     <button id='accBtn' className=" text-zinc-300 font-bold text-xl border-x-2 border-gray-400 h-full px-2">Account</button>
                 </Link>
                 <Link to="/" className='h-full'>
-                    <button id="homeBtn" className=" text-zinc-300 font-bold h-full px-2 text-2xl">TraderJournal</button>
+                    <button id="homeBtn" className=" text-zinc-300 font-bold h-full px-2 text-3xl">TraderJournal</button>
                 </Link>
                 <button id="addBtn" className="text-zinc-300 font-bold text-xl border-x-2 border-gray-400 h-full w-20" onMouseUp={()=>{setAdd((prevAdd) => !prevAdd)}}>Add</button>
             </div>
