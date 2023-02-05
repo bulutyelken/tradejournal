3bab3a4f61b75421eb59c0d92333dbcc3e1f704a
bulutyelken <190101052@ogrenci.yalova.edu.tr>
2023-02-05 18:53:16 +0300

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,16 @@ import {SiGmail} from 'react-icons/si'
 
 export default function Footer(){
     return (
-        <footer className="bg-header h-14 text-gray-200 flex justify-center items-center absolute bottom-0 w-full sm:w-screen">
-            <h1 className="text-center">This website created by <a href="https://github.com/bulutyelken" rel='noreferrer' target="_blank" className="font-bold">Bulut.</a></h1>
-            <div className="absolute right-6 flex flex-row gap-4 mt-1">
-                <a href='https://github.com/bulutyelken' target="_blank" rel='noreferrer'><BsGithub size={30} className="cursor-pointer" /></a>
-                <a href="mailto:bulutyelken@gmail.com">
-                        <SiGmail size={30} className="cursor-pointer"/>
-                </a>
-                <a href='https://linkedin.com/in/bulutyelken' target="_blank" rel='noreferrer'><BsLinkedin size={30} className="cursor-pointer"/></a>
-            </div>
-        </footer>
+        //flex justify-around items-center w-full h-16 bg-header fixed
+            <footer className="flex w-full bottom-0 fixed bg-header h-14 text-gray-200 justify-center items-center">
+                <h1 className="text-center">This website created by <a href="https://github.com/bulutyelken" rel='noreferrer' target="_blank" className="font-bold">Bulut.</a></h1>
+                <div className="absolute right-6 flex flex-row gap-4 mt-1">
+                    <a href='https://github.com/bulutyelken' target="_blank" rel='noreferrer'><BsGithub size={30} className="cursor-pointer" /></a>
+                    <a href="mailto:bulutyelken@gmail.com">
+                            <SiGmail size={30} className="cursor-pointer"/>
+                    </a>
+                    <a href='https://linkedin.com/in/bulutyelken' target="_blank" rel='noreferrer'><BsLinkedin size={30} className="cursor-pointer"/></a>
+                </div>
+            </footer>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@ import {Link} from 'react-router-dom';
 
 function Header({setAdd}){
     return(
-                <div className="flex justify-around items-center w-full h-16 bg-header fixed">
+        <header className='flex justify-around items-center w-full h-16 bg-header fixed'>
+                
                     <Link  to="account" className='h-full'>
                         <button id='accBtn' className=" text-zinc-300 font-bold text-xl border-x-2 border-gray-400 h-full px-2 hover:bg-white hover:text-black hover:border-white transition-all">Account</button>
                     </Link>
@@ -10,7 +11,8 @@ function Header({setAdd}){
                         <button className="homeBtn ease-linear duration-700 delay-500 text-zinc-300 font-bold h-full px-2 text-3xl z-10"><span className='trdr'>Trader</span><span className='jrnl'>Journal</span></button>
                     </Link>
                     <button id="addBtn" className="text-zinc-300 font-bold text-xl border-x-2 border-gray-400 h-full w-20  hover:bg-white hover:text-black hover:border-white transition-all" onMouseUp={()=>{setAdd((prevAdd) => !prevAdd)}}>Add</button>
-                </div>
+                
+        </header>
 
     )
 }
